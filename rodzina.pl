7be6be7d8acefb729397a234eee14277ec8480e1
aleksl11 <110901701+aleksl11@users.noreply.github.com>
2023-10-25 10:29:00 +0200

    Add files via upload

---
+++ rodzina.pl
@@ -0,0 +1,46 @@
+rodzic(zofia, marcin).
+rodzic(andrzej, marcin).
+rodzic(andrzej, kasia).
+rodzic(marcin, ania).
+rodzic(marcin, krzyœ).
+rodzic(krzyœ, miko³aj).
+kobieta(zofia).
+kobieta(kasia).
+kobieta(ania).
+mê¿czyzna(andrzej).
+mê¿czyzna(marcin).
+mê¿czyzna(krzyœ).
+mê¿czyzna(miko³aj).
+potomek(Y,X):-
+	rodzic(X,Y).
+matka(X,Y):-
+	rodzic(X,Y),
+	kobieta(X).
+dziadkowie(X,Z):-
+	rodzic(X,Y),
+	rodzic(Y,Z).
+siostra(X,Y):-
+	rodzic(Z,X),
+	rodzic(Z,Y),
+	kobieta(X).
+
+przodek(X,Y):- rodzic(X,Y).
+przodek(X,Y):- rodzic(X,Z), przodek(Z,Y).
+
+nastepca(X,Y):- przodek(Y,X).
+
+szczesliwy(X):-
+	rodzic(X,_).
+
+dwoje_dzieci(X):-
+	rodzic(X,Y),
+	siostra(Y,_).
+
+wnuk(X,Y):-
+	rodzic(Y,Z),
+	rodzic(Z,X).
+
+ciotka(X,Y):-
+	siostra(X,Z),
+	rodzic(Z,Y).
+
